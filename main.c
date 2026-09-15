84d49cff8c57a16f4659bd6e0e139e0bf3055d48
Salma Abdelmoaty <salmaabdelmoaty5@gmail.com>
2026-09-15 03:48:36 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,73 +1,70 @@
-#include <avr/io.h>
-#include "Headers/Bit_Math.h"
-#include "Headers/Std_types.h"
+#include "Headers/ADC.h"
 #include "Headers/DIO.h"
-#include "Headers/LCD.H"
-#include "Headers/keypad.h"
-#define F_CPU 8000000UL
-#include <util/delay.h>
+#include "Headers/servo_motor.h"
 #include "Headers/USART.h"
+#define F_CPU 8000000UL
+#include <avr/delay.h>
 
 
 int main(void)
 {
-	LCD_init();
-	KEYPAD_init();
+    DIO_voidSetPinDirection(PORTC,PIN0,OUTPUT);
+    DIO_voidSetPinValue(PORTC,PIN0,LOW);
+	DIO_voidSetPinDirection(PORTA,PIN0,INPUT);//Pin for LDR output
+	DIO_voidSetPinDirection(PORTB,PIN0,OUTPUT); // pin for the electronic switch
+	ADC_init();
 	USART_Init(9600);
-	const u8 correctpass[3] = {'1', '2', '3'};
-	u8 attempts=0;
+	ServoMotor_Init();
+	CLR_BIT(DDRA,1);// pin1 in PORTA work as input ->LDR pin
+	SET_BIT(DDRD,5);// pin2 in PORTD work as output->servo motor pin
+	u32 led_ins; // var store the outdoor lighting instensity
+	u8 reciever = '0'; // default state
+	u8 new_cmd = 0;
 	
-	for ( attempts =0 ; attempts <3 ; attempts++){
-		LCD_SendCommand(0b00000001);
-		LCD_SendString((u8 *)"Enter door pass:");
-		GoToXY(0, 1);
-		u8 entered[3];
-		
-		for(u8 enterednum =0 ; enterednum <3 ; enterednum++) {
-			u8 storing = KEYPAD_keypressed();
-			while (storing == '\0') {
-				storing = KEYPAD_keypressed();
-			}
-			if (storing == 'C' ) {
-				_delay_ms(150); 
-				LCD_SendCommand(0b00000001);
-				GoToXY(0, 0);
-				LCD_SendString((u8 *)"Enter door pass:");
-				GoToXY(0, 1);
-				enterednum = 0;
-				_delay_ms(100);
-				continue;
-			}
-			
-			entered[enterednum] = storing;
-			LCD_sendChar(storing);
-			_delay_ms(50);
-		}
-		LCD_SendCommand(0b00000001);
-		GoToXY(0, 0);
-		if (entered[0] == correctpass[0] && entered[1] == correctpass[1] && entered[2] == correctpass[2]) {
-			LCD_SendString((u8 *)"door opened");
-			GoToXY(0, 1);
-			LCD_SendString((u8 *)"Welcome!");
-			
-			USART_SendChar('1');
-			
-			break;
-		}
-		else {
-			LCD_SendString((u8 *)"wrong pass");
-		}
-		_delay_ms(150);
-	}
-	if (attempts >= 3){
-		LCD_SendCommand(0b00000001);
-		GoToXY(0, 0);
-		LCD_SendString((u8 *)"No more trials!");
-		
-		USART_SendChar('2');
-	}
-	while(1)
+	//DDRD &= ~(1 << DDD2);
+
+	//DDRC |= (1 << DDC0);
+	//DPORTC &= ~(1 << PIN0);
+
+	
+	while (1)
 	{
+		// Check for UART command without stopping the loop
+		if (USART_Receive_NoBlock(&new_cmd))
+		{
+			reciever = new_cmd; // Updates to '1' when password is correct, or '0' when locked
+		}
 		
+		if (reciever == '1')
+		{
+			// Continuous Automatic Lighting (LDR -> LED)
+			u16 ldr_light = ADC_Read(0);
+			if (ldr_light >= 350)
+			DIO_voidSetPinValue(PORTB, PIN0, LOW);
+			else
+			DIO_voidSetPinValue(PORTB, PIN0, HIGH);
+			
+			// Continuous Curtain Control (LDR -> Servo)
+			u16 ldr_curtain = ADC_Read(1);
+			if (ldr_curtain > 400)
+			ServoMotor_OCA1_Angle(180);
+			else
+			ServoMotor_OCA1_Angle(0);
+		}
+		else if(reciever == '2')
+		{
+			DIO_voidSetPinValue(PORTC,PIN0,HIGH);
+		}
+		else
+		{
+			
+		}
 	}
 }
+	
+  
+		
+		
+		
+
+
